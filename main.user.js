d4766b95a4c42f1e3de01c2789da7dbe0a6e71ca
Yordan Grigorov <156067529+dako-23@users.noreply.github.com>
2025-12-16 17:14:34 +0200

    Update version and descriptions in user script

--- main.user.js
+++ main.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Mega AI Phrase
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  MAIPH v.1.0.0
+// @version      1.0.1
+// @description  MAIPH v.1.0.1
 // @author       DAKO23
 // @match        https://megasys.megaparts.bg/megasys/product*
 // @icon         https://cdn.creazilla.com/cliparts/3171394/gear-clipart-lg.png
 
-// @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/utils.js
+// @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/utils.js?v=1
 // @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/phrasePipeline.js?v=1
-// @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/collectData.js
+// @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/collectData.js?v=1
 // @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/editAutoFill.js?v=1
-// @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/buttons/buttons.js
+// @require      https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/buttons/buttons.js?v=1
 
 // @resource     CUSTOMCSS https://raw.githubusercontent.com/dako-23/MegaAiPhrase/main/src/buttons/styles.css
 
